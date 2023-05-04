9a73697f0fc66a1f158c993cea5837f4533b6432
Federico Nery <fedegnery@gmail.com>
2023-05-04 16:33:01 -0300

    master :: cambio en DigimonStatsBaseMock

--- mocks/DigimonStatsBase.mock.ts
+++ mocks/DigimonStatsBase.mock.ts
@@ -1,5 +1,5 @@
 import { DigimonStatsBase } from "domain/digimon/DigimonStatsBase";
 
 export class DigimonStatsBaseMock {
-    static STATS_AGUMON: DigimonStatsBase = new DigimonStatsBase
+    static STATS_AGUMON: DigimonStatsBase = new DigimonStatsBase(60, 40, 10, 10, 10, 250, 10)
 }
